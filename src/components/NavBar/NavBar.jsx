e67d4cfdcd42959fa63bdca441c1818530116ab5
Jorge Campostano <j.campostano@gmail.com>
2023-11-12 15:19:12 -0300

    Initial commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+
+    <h3>title</h3>
+    <div>
+        <button>muebles</button>
+        <button>cuadros</button>
+        <button>relojes</button>
+    </div>
+
+    </nav>
+  )
+}
+
+export default NavBar
